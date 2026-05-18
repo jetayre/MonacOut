86724ba76f27d4f68c08eb4573ba68c6519c0baa
Stéphanie Ayre <jetayre@github.com>
2026-05-18 19:39:13 +0200

    design: logo Monac or + Out navy, sans italique

--- src/components/MonacOutLogo.jsx
+++ src/components/MonacOutLogo.jsx
@@ -1,19 +1,20 @@
 const GOLD = "#C4A241";
+const NAVY = "#0F1D3A";
 
 export default function MonacOutLogo({ width = 290 }) {
   const scale = width / 290;
+  const style = {
+    fontFamily: "'Playfair Display', Georgia, serif",
+    fontSize: Math.round(52 * scale),
+    fontWeight: 700,
+    fontStyle: "normal",
+    letterSpacing: Math.round(2 * scale),
+    lineHeight: 1,
+  };
   return (
     <div style={{ textAlign: "center", padding: `${8 * scale}px 0 ${6 * scale}px` }}>
-      <div style={{
-        fontFamily: "'Playfair Display', Georgia, serif",
-        fontSize: Math.round(52 * scale),
-        fontWeight: 700,
-        fontStyle: "italic",
-        color: GOLD,
-        letterSpacing: Math.round(2 * scale),
-        lineHeight: 1,
-      }}>
-        MonacOut
+      <div style={{ ...style, display: "inline" }}>
+        <span style={{ color: GOLD }}>Monac</span><span style={{ color: NAVY }}>Out</span>
       </div>
     </div>
   );
