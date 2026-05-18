a785ad86635c83fdf43c982702491972a0d33e29
Stéphanie Ayre <jetayre@github.com>
2026-05-18 19:41:37 +0200

    design: logo plus grand, tout en navy

--- src/components/MonacOutLogo.jsx
+++ src/components/MonacOutLogo.jsx
@@ -5,7 +5,7 @@ export default function MonacOutLogo({ width = 290 }) {
   const scale = width / 290;
   const style = {
     fontFamily: "'Great Vibes', cursive",
-    fontSize: Math.round(52 * scale),
+    fontSize: Math.round(72 * scale),
     fontWeight: 700,
     fontStyle: "normal",
     letterSpacing: Math.round(2 * scale),
@@ -14,7 +14,7 @@ export default function MonacOutLogo({ width = 290 }) {
   return (
     <div style={{ textAlign: "center", padding: `${8 * scale}px 0 ${6 * scale}px` }}>
       <div style={{ ...style, display: "inline" }}>
-        <span style={{ color: GOLD }}>Monac</span><span style={{ color: NAVY }}>Out</span>
+        <span style={{ color: NAVY }}>MonacOut</span>
       </div>
     </div>
   );
