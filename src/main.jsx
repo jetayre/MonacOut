347b95a316627266d410a163091c3f59af1637f8
Stéphanie Ayre <jetayre@github.com>
2026-05-19 16:13:59 +0200

    feat: ajout PostHog analytics (EU)

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import posthog from 'posthog-js'
+
+posthog.init('phc_qfThmficvfkSEgsMLbKiJcgiHRYyAJ5GU2i8pavYYzNU', {
+  api_host: 'https://eu.i.posthog.com',
+  person_profiles: 'identified_only',
+})
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
